0991a1ab07deac7945b044444e70356d0f4f9244
adiyoga <adiyoga27new@gmail.com>
2024-08-10 16:12:04 +0000

    integer

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'is_smoke' => 'boolean',
+            'is_smoke' => 'integer',
         ];
     }
 }
