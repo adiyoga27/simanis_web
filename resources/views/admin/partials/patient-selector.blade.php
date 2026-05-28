83ed442f1c0bc55c6e193910ebf99aa18b725c6f
adiyoga <adiyoga27new@gmail.com>
2026-05-28 20:16:31 +0800

    feat: add conditional rendering for patient selector based on user role

--- resources/views/admin/partials/patient-selector.blade.php
+++ resources/views/admin/partials/patient-selector.blade.php
@@ -1,3 +1,4 @@
+@if(Auth::user()->role !== 'kepala_puskesmas')
 @php
 $redirectTo = $redirectTo ?? '';
 $buttonLabel = $buttonLabel ?? 'Input Data';
@@ -10,3 +11,4 @@ $backUrl = $backUrl ?? url()->current();
     </svg>
     {{ $buttonLabel }}
 </a>
+@endif
